6539ddda56e11b6958e4c98010e6cca5170573e3
lathifarsy <lathifarsy@gmail.com>
2021-05-06 21:00:11 +0700

    share code on github

---
+++ src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name: 'matriks',
+            category:'math',
+            image:'/images/p1.JPG',
+            price:'89,000',
+            brand:'fruitless',
+            rating:4.5,
+            numReviews:14,
+            desc:'matriks of fruit'
+        },
+        {
+            _id:'2',
+            name: 'matriks',
+            category:'math',
+            image:'/images/p1.JPG',
+            price:'145,000',
+            brand:'fruitless',
+            rating:4.5,
+            numReviews:14,
+            desc:'matriks of fruit'
+        },
+        {
+            _id:'1',
+            name: 'matriks',
+            category:'math',
+            image:'/images/p1.JPG',
+            price:'45,000',
+            brand:'fruitless',
+            rating:4.5,
+            numReviews:14,
+            desc:'matriks of fruit'
+        },
+        {
+            _id:'1',
+            name: 'matriks',
+            category:'math',
+            image:'/images/p1.JPG',
+            price:'99,000',
+            brand:'fruitless',
+            rating:4.5,
+            numReviews:14,
+            desc:'matriks of fruit'
+        },
+        {
+            _id:'1',
+            name: 'matriks',
+            category:'math',
+            image:'/images/p1.JPG',
+            price:'19,000',
+            brand:'fruitless',
+            rating:4.5,
+            numReviews:14,
+            desc:'matriks of fruit'
+        },
+        {
+            _id:'1',
+            name: 'matriks',
+            category:'math',
+            image:'/images/p1.JPG',
+            price:'21,000',
+            brand:'fruitless',
+            rating:4.5,
+            numReviews:14,
+            desc:'matriks of fruit'
+        }
+    ]
+}
+
+export default data;
